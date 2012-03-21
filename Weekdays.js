095121d45ded5cd58c6689efe9d9d58ba97ea82e
Timm Friebe <thekid@thekid.de>
2012-03-21 14:38:15 +0100

    - Move weekdays stuff to demo/ # To run, use node tools/node.js demo.Weekdays

--- Weekdays.js
+++
@@ -1,5 +0,0 @@
-uses('Weekday', 'util.cmd.Console');Weekdays= define('Weekdays','lang.Object',function Weekdays(){});
-
-Weekdays.main= function Weekdays$main(args){
-Weekday.values().forEach(function(day) {
-util.cmd.Console.writeLine(day,' weekend? ',day.isWeekend());});};
