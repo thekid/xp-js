a373e0958b733e41819a91eb864c2d97ff0b2cbb
Timm Friebe <thekid@thekid.de>
2012-03-23 16:43:08 +0100

    - Make more similar to `xp -r`

--- Reflect.js
+++ Reflect.js
@@ -8,12 +8,21 @@ Reflect.prototype.toString = function Reflect$toString() {
 Reflect.main = function Reflect$main(args) {
   clazz= lang.XPClass.forName(args[0]);
 
-  util.cmd.Console.writeLine(clazz);
-  util.cmd.Console.writeLine('Methods: ', clazz.getMethods());
-  util.cmd.Console.writeLine('Fields:  ', clazz.getFields());
+  util.cmd.Console.write(clazz);
+  if (null !== (parent= clazz.getParentclass())) {
+    util.cmd.Console.writeLine(' extends ' + parent.getName() + ' {');
+  } else {
+    util.cmd.Console.writeLine(' {');
+  }
 
-  instance= clazz.newInstance();
-  util.cmd.Console.writeLine(instance);
-  util.cmd.Console.writeLine(instance.getClass());
+  clazz.getFields().forEach(function(field) {
+    util.cmd.Console.writeLine('  ' + field.toString());
+  });
+  util.cmd.Console.writeLine();
+  clazz.getMethods().forEach(function(method) {
+    util.cmd.Console.writeLine('  ' + method.toString());
+  });
+
+  util.cmd.Console.writeLine(' }');
 }
 // }}}
