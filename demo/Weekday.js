1afa0ca32b086665e12e1deeb03d5db39b1eeb00
Timm Friebe <thekid@thekid.de>
2012-03-23 13:40:11 +0100

    - Initial release: demo folder

---
+++ demo/Weekday.js
@@ -0,0 +1,13 @@
+uses('lang.Enum');
+
+demo.Weekday= define('demo.Weekday','lang.Enum',function Weekday(){lang.Enum.apply(this, arguments);});
+Object.defineProperty(demo.Weekday,'Monday',{ writeable : false, value : new demo.Weekday(0,'Monday')})
+Object.defineProperty(demo.Weekday,'Tuesday',{ writeable : false, value : new demo.Weekday(1,'Tuesday')})
+Object.defineProperty(demo.Weekday,'Wednesday',{ writeable : false, value : new demo.Weekday(2,'Wednesday')})
+Object.defineProperty(demo.Weekday,'Thursday',{ writeable : false, value : new demo.Weekday(3,'Thursday')})
+Object.defineProperty(demo.Weekday,'Friday',{ writeable : false, value : new demo.Weekday(4,'Friday')})
+Object.defineProperty(demo.Weekday,'Saturday',{ writeable : false, value : new demo.Weekday(5,'Saturday')})
+Object.defineProperty(demo.Weekday,'Sunday',{ writeable : false, value : new demo.Weekday(6,'Sunday')})
+
+demo.Weekday.prototype.isWeekend= function Weekday$isWeekend(){
+return this._ordinal > 4;};demo.Weekday.values= function Weekday$values(){return lang.Enum.membersOf(this);};
---
+++ demo/Weekdays.js
@@ -0,0 +1,7 @@
+uses('demo.Weekday', 'util.cmd.Console');
+
+demo.Weekdays= define('demo.Weekdays','lang.Object',function Weekdays(){});
+
+demo.Weekdays.main= function Weekdays$main(args){
+demo.Weekday.values().forEach(function(day) {
+util.cmd.Console.writeLine(day,' weekend? ',day.isWeekend());});};
