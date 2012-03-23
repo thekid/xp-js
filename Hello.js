f9baaea6f12a2415eb579722b465a30ecbd0e94b
Timm Friebe <thekid@thekid.de>
2012-03-23 13:54:30 +0100

    - Revert back from compiled form to handcoded equivalent

--- Hello.js
+++ Hello.js
@@ -1,9 +1,10 @@
-uses('util.cmd.Console');;
+// {{{ Hello
+Hello = define('Hello', 'lang.Object', function Hello () { });
 
-Hello= define('Hello','lang.Object',function Hello(){});
-
-Hello.main= function Hello$main(args){
-if (args.length < 1) {
-throw new lang.IllegalArgumentException('Argument required');};
-
-util.cmd.Console.writeLine('Hello ',global.native.ucfirst(args[0]));};
+Hello.main = function Hello$main(args) {
+  if (args.length < 1) {
+    throw new lang.IllegalArgumentException('Argument required');
+  }
+  util.cmd.Console.writeLine('Hello ', global.native.ucfirst(args[0]));
+}
+// }}}
