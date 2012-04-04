1e5276d8a715af7b7848001d25d5005df15454b7
Timm Friebe <thekid@thekid.de>
2012-04-04 19:10:48 +0200

    - Fix non-continuous [T extends Enum]::values()

--- core/src/main/js/lang/Enum.js
+++ core/src/main/js/lang/Enum.js
@@ -5,19 +5,11 @@ lang.Enum = define('lang.Enum', 'lang.Object', function Enum($ordinal, $name) {
 
 lang.Enum.membersOf = function Enum$membersOf(self) {
   var names = Object.getOwnPropertyNames(self);
-  result= [];
+  members= [];
   for (var i = 0; i < names.length; i++) {
     prop= self[names[i]];
-    if ('prototype' !== names[i] && prop instanceof lang.Enum) result[prop.$ordinal]= prop;
+    if ('prototype' !== names[i] && prop instanceof lang.Enum) members[prop.$ordinal]= prop;
   }
-  return result;
-}
-
-lang.Enum.valuesOf = function Enum$valuesOf($class) {
-  if (!$class.isEnum()) {
-    throw new lang.IllegalArgumentException($class.getName() + ' is not an enum');
-  }
-  var members = lang.Enum.membersOf($class.$reflect);
 
   // The members array is sorted by ordinal, but has space
   // e.g. "a,b,,,,,c,d" if the ordinals are not continuous 
@@ -26,6 +18,13 @@ lang.Enum.valuesOf = function Enum$valuesOf($class) {
     result.push(members[member]);
   }
   return result;
+}
+
+lang.Enum.valuesOf = function Enum$valuesOf($class) {
+  if (!$class.isEnum()) {
+    throw new lang.IllegalArgumentException($class.getName() + ' is not an enum');
+  }
+  return lang.Enum.membersOf($class.$reflect);
 }
 
 lang.Enum.valueOf = function Enum$valueOf($class, $name) {
