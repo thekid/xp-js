4ff4fd6b226f87908dc41531414678cdb68f3948
Timm Friebe <thekid@thekid.de>
2012-04-04 10:40:59 +0200

    - Recompile

--- demo/Operation.js
+++ demo/Operation.js
@@ -10,16 +10,16 @@ uses('lang.Enum', 'util.cmd.Console');
 
 demo.Operation= define('demo.Operation','lang.Enum',function Operation(){lang.Enum.apply(this, arguments);});demo.Operation['_']= {throws: [],signature: []};
 demo.Operation$$plus=define('demo.Operation$$plus','demo.Operation',function plus() {demo.Operation.apply(this, arguments);});
-demo.Operation$$plus.prototype.evaluate= function demo$Operation$$plus$evaluate($x,$y){return $x+$y;};demo.Operation$$plus.prototype.evaluate['_']= {returns: 'int',throws: [],signature: ['int','int',]};;Object.defineProperty(demo.Operation,'plus',{ writeable : false, value : new demo.Operation$$plus(0,'plus')})
+demo.Operation$$plus.prototype.evaluate= function demo$Operation$$plus$evaluate($x,$y){return $x+$y;};demo.Operation$$plus.prototype.evaluate['_']= {returns: 'int',throws: [],signature: ['int','int',]};;Object.defineProperty(demo.Operation,'$plus',{ writeable : false, value : new demo.Operation$$plus(0,'plus')});
 
 demo.Operation$$minus=define('demo.Operation$$minus','demo.Operation',function minus() {demo.Operation.apply(this, arguments);});
-demo.Operation$$minus.prototype.evaluate= function demo$Operation$$minus$evaluate($x,$y){return $x-$y;};demo.Operation$$minus.prototype.evaluate['_']= {returns: 'int',throws: [],signature: ['int','int',]};;Object.defineProperty(demo.Operation,'minus',{ writeable : false, value : new demo.Operation$$minus(1,'minus')})
+demo.Operation$$minus.prototype.evaluate= function demo$Operation$$minus$evaluate($x,$y){return $x-$y;};demo.Operation$$minus.prototype.evaluate['_']= {returns: 'int',throws: [],signature: ['int','int',]};;Object.defineProperty(demo.Operation,'$minus',{ writeable : false, value : new demo.Operation$$minus(1,'minus')});
 
 demo.Operation$$times=define('demo.Operation$$times','demo.Operation',function times() {demo.Operation.apply(this, arguments);});
-demo.Operation$$times.prototype.evaluate= function demo$Operation$$times$evaluate($x,$y){return $x*$y;};demo.Operation$$times.prototype.evaluate['_']= {returns: 'int',throws: [],signature: ['int','int',]};;Object.defineProperty(demo.Operation,'times',{ writeable : false, value : new demo.Operation$$times(2,'times')})
+demo.Operation$$times.prototype.evaluate= function demo$Operation$$times$evaluate($x,$y){return $x*$y;};demo.Operation$$times.prototype.evaluate['_']= {returns: 'int',throws: [],signature: ['int','int',]};;Object.defineProperty(demo.Operation,'$times',{ writeable : false, value : new demo.Operation$$times(2,'times')});
 
 demo.Operation$$divided_by=define('demo.Operation$$divided_by','demo.Operation',function divided_by() {demo.Operation.apply(this, arguments);});
-demo.Operation$$divided_by.prototype.evaluate= function demo$Operation$$divided_by$evaluate($x,$y){return $x/$y;};demo.Operation$$divided_by.prototype.evaluate['_']= {returns: 'int',throws: [],signature: ['int','int',]};;Object.defineProperty(demo.Operation,'divided_by',{ writeable : false, value : new demo.Operation$$divided_by(3,'divided_by')})
+demo.Operation$$divided_by.prototype.evaluate= function demo$Operation$$divided_by$evaluate($x,$y){return $x/$y;};demo.Operation$$divided_by.prototype.evaluate['_']= {returns: 'int',throws: [],signature: ['int','int',]};;Object.defineProperty(demo.Operation,'$divided_by',{ writeable : false, value : new demo.Operation$$divided_by(3,'divided_by')});
 
 
 
--- demo/Weekday.js
+++ demo/Weekday.js
@@ -1,13 +1,13 @@
 uses('lang.Enum');
 
 demo.Weekday= define('demo.Weekday','lang.Enum',function Weekday(){lang.Enum.apply(this, arguments);});demo.Weekday['_']= {throws: [],signature: []};
-Object.defineProperty(demo.Weekday,'Monday',{ writeable : false, value : new demo.Weekday(0,'Monday')})
-Object.defineProperty(demo.Weekday,'Tuesday',{ writeable : false, value : new demo.Weekday(1,'Tuesday')})
-Object.defineProperty(demo.Weekday,'Wednesday',{ writeable : false, value : new demo.Weekday(2,'Wednesday')})
-Object.defineProperty(demo.Weekday,'Thursday',{ writeable : false, value : new demo.Weekday(3,'Thursday')})
-Object.defineProperty(demo.Weekday,'Friday',{ writeable : false, value : new demo.Weekday(4,'Friday')})
-Object.defineProperty(demo.Weekday,'Saturday',{ writeable : false, value : new demo.Weekday(5,'Saturday')})
-Object.defineProperty(demo.Weekday,'Sunday',{ writeable : false, value : new demo.Weekday(6,'Sunday')})
+Object.defineProperty(demo.Weekday,'$Monday',{ writeable : false, value : new demo.Weekday(0,'Monday')});
+Object.defineProperty(demo.Weekday,'$Tuesday',{ writeable : false, value : new demo.Weekday(1,'Tuesday')});
+Object.defineProperty(demo.Weekday,'$Wednesday',{ writeable : false, value : new demo.Weekday(2,'Wednesday')});
+Object.defineProperty(demo.Weekday,'$Thursday',{ writeable : false, value : new demo.Weekday(3,'Thursday')});
+Object.defineProperty(demo.Weekday,'$Friday',{ writeable : false, value : new demo.Weekday(4,'Friday')});
+Object.defineProperty(demo.Weekday,'$Saturday',{ writeable : false, value : new demo.Weekday(5,'Saturday')});
+Object.defineProperty(demo.Weekday,'$Sunday',{ writeable : false, value : new demo.Weekday(6,'Sunday')});
 
 demo.Weekday.prototype.isWeekend= function Weekday$isWeekend(){
 return this.$ordinal > 4;};demo.Weekday.prototype.isWeekend['_']= {returns: 'bool',throws: [],signature: []};demo.Weekday.values= function Weekday$values(){return lang.Enum.membersOf(this);};demo.Weekday.values['/']= "erate";demo.Weekday.values['_']= {returns: 'demo.Weekday[]',throws: [],signature: []};
