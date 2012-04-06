a0f8b3bc89d5724bfaf22bb49cd58ef55adb58e2
Timm Friebe <thekid@thekid.de>
2012-04-06 15:57:53 +0200

    - Initial release

---
+++ core/src/main/js/lang/archive/ArchiveClassLoader.js
@@ -0,0 +1,42 @@
+lang.archive.ArchiveClassLoader = define('lang.archive.ArchiveClassLoader', 'lang.Object', function ArchiveClassLoader($path) {
+  this.$path= $path;
+  this.$xar= null;
+});
+
+lang.archive.ArchiveClassLoader.prototype.providesClass = function ArchiveClassLoader$providesClass($class) {
+  if (this.$xar === null) {
+    this.$xar= global.xar.acquire(this.$path);
+  }
+
+  return this.$xar.index[$class.replace(/\./g, '/') + '.js'] !== undefined;
+}
+
+lang.archive.ArchiveClassLoader.prototype.loadClass0 = function ArchiveClassLoader$loadClass0($class) {
+  if (this.$xar === null) {
+    this.$xar= global.xar.acquire(this.$path);
+  }
+
+  var ln = $class.replace(/\./g, '/') + '.js';
+  if (this.$xar.index[ln] === undefined) {
+    throw new lang.ClassNotFoundException('No such class ' + $class);
+  }
+
+  var names = $class.split('.');
+  var it = global;
+  for (var n= 0; n < names.length - 1; n++) {
+    if (typeof(it[names[n]]) === 'undefined') it[names[n]]= {};
+    it = it[names[n]];
+  }
+
+  eval(this.$xar.index[ln].read().toString());
+  global[$class]= it[names[n]]= eval($class);
+  if (typeof(it[names[n]]['__static']) === 'function') {
+    it[names[n]].__static();
+  }
+  
+  return $class;
+}
+
+lang.archive.ArchiveClassLoader.prototype.toString = function ArchiveClassLoader$toString() {
+  return this.getClassName() + '<' + this.$path + '>';
+}
